e7e19136415f065d566d5a3b034df3d26f9ad2fc
gorkamagda <gorka.magda@gmail.com>
2017-10-20 12:43:36 +0200

    Add files via upload

---
+++ LAB1_Source.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include "perceptron.h"
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+void wczytaj_dane(Neuron n1,double **tw, int n);
+double korekcja_w1(double p,Neuron n1);
+double korekcja_w2(double p,Neuron n1);
+
+
+int main(){
+	Neuron n1;
+	n1.losuj_wagi();
+	int n;
+	cout<<"Podaj ilosc danych do 20: ";
+	cin>>n;
+	cout<<endl;
+	double **tab_wejscia = new double *[n];
+	for(int i=0; i<n;i++){
+		tab_wejscia[i]=new double [3];
+	}
+	cout<<"WAGI poczatkowe: ";
+	cout<<n1.get_w1()<<" "<<n1.get_w2()<<endl;
+	
+	wczytaj_dane(n1,tab_wejscia,n);
+	bool warunek = true;
+	int i=1;
+	while(warunek){
+		cout<<"__________________________ EPOKA "<<i<<"_________________________"<<endl;
+		int licz=0;
+		for(int j=0;j<n;j++)
+		{
+			//cout<<" X1 przed : "<<n1.get_x1()<<endl;
+			//cout<<" X2 przed : "<<n1.get_x2()<<endl;
+			n1.set_x1(tab_wejscia[j][0]);
+			n1.set_x2(tab_wejscia[j][1]);
+			//cout<<" X1 po : "<<n1.get_x1()<<endl;
+			//cout<<" X2 po : "<<n1.get_x2()<<endl;
+						
+			if((fabs(n1.fun_act(n1.suma())-tab_wejscia[j][2])>0.99)||(fabs(tab_wejscia[j][2]-n1.fun_act(n1.suma()))<0.1))
+			{			
+				licz++;
+				cout<<"Funkcja aktywacji: "<<n1.fun_act(n1.suma())<<"  Rekord nauczony !"<<endl;	
+				if(licz==n){
+					cout<<"Neuron nauczyl sie za "<<i+1<<" razem"<<endl;
+					warunek=false;}
+			}
+			else
+			{	cout<<"Funkcja aktywacji: "<<n1.fun_act(n1.suma());
+				korekcja_w1(tab_wejscia[j][2]-n1.fun_act(n1.suma()),n1);
+				korekcja_w1(tab_wejscia[j][2]-n1.fun_act(n1.suma()),n1);
+				n1.set_w1(n1.get_w1()+korekcja_w1(tab_wejscia[j][2]-n1.fun_act(n1.suma()),n1));
+				n1.set_w2(n1.get_w2()+korekcja_w2(tab_wejscia[j][2]-n1.fun_act(n1.suma()),n1));
+				cout<<" w1: "<<n1.get_w1()<<" , w2: "<<n1.get_w2()<<endl;
+				
+			}
+		}
+		i++;
+	}
+system("PAUSE");
+}
+
+void wczytaj_dane(Neuron n1,double **tw, int n){
+	
+	int z=0,j=0,nr_lini=0;
+	string linia,pom;
+	fstream plik;	
+
+	plik.open("dane.txt", ios::in);
+
+	if(plik.good()==false)
+	{
+		cout<<"Blad wczytywania pliku !!!"<<endl;
+		exit(0);
+	}
+	
+	while(getline(plik,linia))
+	{ 
+		for(unsigned int i=0; i < linia.length();i++)
+				{
+					if((linia[i]==' ') && (j==0))
+					{
+						pom.insert(0,linia,z,i);				
+						tw[nr_lini][0]=(atof(pom.c_str()));
+						j++;
+						z=i;
+						pom.clear();
+					}
+					else
+						if((linia[i]==' ') && (j==1))
+					{
+						pom.insert(0,linia,z,i-z);
+						tw[nr_lini][1]=(atof(pom.c_str()));
+						j++;
+						z=i;
+						pom.clear();
+					}
+					else
+					if(i==linia.length()-1 && j==2)
+					{
+						pom.insert(0,linia,z,i-z+1);
+						tw[nr_lini][2]=atoi(pom.c_str());
+						j=0;
+						z=0;
+						pom.clear();
+					}
+				}
+		nr_lini++;
+		if(nr_lini==n){break;}
+		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<3;j++)
+				cout<< tw[i][j]<<"  ";
+		cout<<endl;
+		}
+}
+double korekcja_w1(double r,Neuron n1){
+	//cout<<"Funkcja aktywacji: "<<n1.fun_act(n1.suma());
+	double c=0.1;//stala uczenia
+	double dw1=c*r*n1.get_x1();//o ile sie zmieni waga1
+	//cout<<"  W1 przed: "<<n1.w1;
+	n1.w1=n1.w1+dw1;
+	//cout<<"  W1 po: "<<n1.w1;
+		return  dw1;
+}
+double korekcja_w2(double r,Neuron n1){
+	//cout<<"Funkcja aktywacji: "<<n1.fun_act(n1.suma());
+	double c=0.2;//stala uczenia
+	double dw2=c*r*n1.get_x2();//o ile sie zmieni waga2
+	//cout<<"  W2 przed: "<<n1.w2;
+	n1.w2=n1.w2+dw2;
+	//cout<<"  W2 po: "<<n1.w2<<endl;
+		return  dw2;
+}
---
+++ LAB1_perceptron.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <time.h>
+#include "perceptron.h"
+using namespace std;
+
+void Neuron::set_w1( double n1 )
+    {
+        w1 = n1;
+    }
+void Neuron::set_w2( double n2 )
+    {
+        w2 = n2;
+    }
+void Neuron::set_x1( double a1 )
+    {
+        x1 = a1;
+    }
+void Neuron::set_x2( double a2 )
+    {
+        x2 = a2;
+    }
+double Neuron::get_w1(){
+		return w1;
+	}
+	double Neuron::get_w2(){
+		return w2;
+	}
+	double Neuron::get_x1(){
+		return x1;
+	}
+	double Neuron::get_x2(){
+		return x2;
+	}
+double Neuron::fun_act(double suma){
+	return (2/(1+exp(-2*0.2*suma)) - 1); //B - wspolczynnik uczenia (0.1 lub 0.5), tangens hiperboliczny - zwraca wartosci od -1 do 1
+}
+void Neuron::losuj_wagi(){
+		srand(time(NULL));
+			double w_1=(rand() % 100);//przedzial wag dla uproszczenia od -2 do 2
+			double w_2=(rand() % 100);
+			set_w1(w_1/100);
+			set_w2(w_2/100);
+	}
+double Neuron::suma(){
+	double sum;
+	sum=(get_x1()*get_w1())+(get_x2()*get_w2());
+	return sum;
+}
+
+
