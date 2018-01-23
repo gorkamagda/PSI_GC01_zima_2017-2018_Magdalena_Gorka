ba6aa42fba475b206e8543130eedd05676377f84
gorkamagda <gorka.magda@gmail.com>
2018-01-23 11:22:42 +0100

    Add files via upload

---
+++ LAB2_Perceptron.h
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+
+using namespace std;
+
+class Perceptron
+{
+	int **Litery;  
+	float *wagi;	
+	int wyniki[20];	
+	int ilosc_wejsc;	
+	bool wynik;	
+	float wsp_nauki;	
+
+public:
+	Perceptron();
+
+	void wczytywanie_danych_z_pliku();
+	float f_sumowania(int i); 
+	void ucze();	
+	void sprawdzam(int tab[]);	
+
+
+
+	~Perceptron();	
+};
